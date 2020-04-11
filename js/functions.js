c30cd0b7285e6dce3c20c19392ae44c387d1ac9b
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-04-11 21:17:40 +0200

    Final commit, submitted to CS50 as well in this version

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,25 @@
 function websiteCount() {
-    websitesPerMinute = 380;
-    websitesPerDay = websitesPerMinute * 60 * 24;
-    msg = "Approximately " + websitesPerMinute + " websites are created every minute! That makes about " + websitesPerDay + " websites per day!";
-    alert(msg);
+  websitesPerMinute = 380;
+  websitesPerDay = websitesPerMinute * 60 * 24;
+  msg = "Approximately " + websitesPerMinute + " websites are created every minute! That makes about " + websitesPerDay + " websites per day!";
+  alert(msg);
+}
+
+function js() {
+  setTimeout(function() {
+    document.querySelector(".fun").innerHTML = "JS";
+  }, 3000);
+  // // make JavaScript Heading to JS
+  // document.querySelector(".fun").innerHTML = "JS";
+}
+
+function fun() {
+  document.querySelector(".navbar").style.visibility = "hidden";
+  document.querySelector(".jumbotron").style.backgroundColor = "pink";
+  document.querySelector(".fun").innerHTML = "LOL";
+  setTimeout(function() {
+    document.querySelector(".navbar").style.visibility = "visible";
+    document.querySelector(".jumbotron").style.backgroundColor = "#e9ecef";
+    document.querySelector(".fun").innerHTML = "JS";
+  }, 5000);
 }
